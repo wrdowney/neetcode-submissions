661cff07ff73d9ee9649dcab44aaa01045ba15e6
William Downey <76638578+wrdowney@users.noreply.github.com>
2026-04-24 08:26:10 -0400

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var lastSeenAt = [Character: Int]()
+        let chars = Array(s)
+        var l = 0, res = 0, r = 0
+        while r < chars.count {
+            if let lastPos = lastSeenAt[chars[r]] {
+                l = max(lastPos + 1, l)
+            }
+
+            lastSeenAt[chars[r]] = r
+            res = max(res, r - l + 1)
+            r += 1
+        }
+
+        return res
+    }
+}
