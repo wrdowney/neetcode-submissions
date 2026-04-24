435f91074ca05b3e98ba83c22c0b7e70c266cb1d
William Downey <76638578+wrdowney@users.noreply.github.com>
2026-04-24 08:50:19 -0400

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.swift
@@ -0,0 +1,52 @@
+class Solution {
+    func characterReplacement(_ s: String, _ k: Int) -> Int {
+        print("HERE")
+        var characterCounts = [Character: Int]()
+        var l = 0, maxf = 0, res = 0
+        let characters = Array(s)
+        for r in 0..<characters.count {
+            characterCounts[characters[r], default: 0] += 1
+            maxf = max(maxf, characterCounts[characters[r]]!)
+
+            if (r - l + 1) - maxf > k {
+                characterCounts[characters[l], default: 0] -= 1
+                l += 1
+            }
+            res = max(res, r - l + 1)
+        }
+        return res
+        /*
+        start w fast and slow ptrs
+        keep track of num replacements
+        check if char at fast is equal to char at slow
+        set new max
+        if equal increment fast
+        */
+        // var res = 0
+        // let charSet = Set(s)
+        // let chars = Array(s)
+
+        // for char in charSet {
+        //     var l = 0, count = 0
+        //     for r in 0..<chars.count {
+        //         // find the number of characters that match c
+        //         if chars[r] == char {
+        //             count += 1
+        //         }
+
+        //         // if characters that don't match c > k, shrink from left
+        //         while (r - l + 1) - count > k {
+        //             // if we remove a c character from window decrease our count
+        //             if chars[l] == char {
+        //                 count -= 1
+        //             }
+        //             l += 1
+        //         }
+
+        //         res = max(res, r - l + 1)
+        //     }
+        // }
+
+        // return res
+    }
+}
